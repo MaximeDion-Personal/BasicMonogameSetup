ed1ed47c944002f0deb1975e9e06f4807cdb1401
MaximeDion-Personal <103292606+MaximeDion-Personal@users.noreply.github.com>
2025-03-08 16:39:17 -0500

    Add files via upload
    Initial commit

---
+++ Initializer.cs
@@ -0,0 +1,20 @@
+﻿using Mouse_Avoider.Source.Core;
+using Mouse_Avoider.Source.Core.Managers;
+using Mouse_Avoider.Source.Screens;
+
+namespace Mouse_Avoider;
+
+public static class Initializer
+{
+	public static void Initialize()
+	{
+		DrawManager.Initialize(Main.GraphicsManager);
+		GameWorld.InitializeWorld();
+
+		var mainScreen = new MainScreen(GameWorld.Instance);
+		ScreenManager.RegisterScreen(mainScreen);
+		ScreenManager.SetStartingScreen(mainScreen);
+
+		GameWorld.Instance.Children.Add(mainScreen);
+	}
+}
---
+++ Main.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+using Mouse_Avoider.Source.Core;
+using Mouse_Avoider.Source.Core.Managers;
+
+namespace Mouse_Avoider;
+
+public class Main : Game
+{
+	public static GraphicsDeviceManager GraphicsManager;
+	public static ContentManager ContentManager;
+	public static Main Instance;
+
+	public Main()
+	{
+		GraphicsManager = new GraphicsDeviceManager(this);
+		ContentManager = Content;
+		Content.RootDirectory = "Content";
+		IsMouseVisible = true;
+		Instance = this;
+	}
+
+	protected override void Initialize()
+	{
+		Initializer.Initialize();
+		base.Initialize();
+	}
+
+	protected override void LoadContent()
+	{
+		// Should I have a loadContentWorld also?
+		//GameWorld.LoadContentWorld(gameTime);
+	}
+
+	protected override void Update(GameTime gameTime)
+	{
+		GameWorld.UpdateWorld(gameTime);
+	}
+
+	protected override void Draw(GameTime gameTime)
+	{
+		DrawManager.Instance.Draw(gameTime);
+		base.Draw(gameTime);
+	}
+
+	public static void ExitGame()
+	{
+		Instance.Exit();
+	}
+}
